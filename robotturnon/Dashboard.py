19d8a2e5c0d68752edeff17513edbe56439047c6
aluebke19 <32465243+aluebke19@users.noreply.github.com>
2018-02-08 10:44:34 -0600

    Update Dashboard.py

--- robotturnon/Dashboard.py
+++ robotturnon/Dashboard.py
@@ -4,9 +4,8 @@ import post_to_web as PTW # see post_to_web.py for instructions
 print "done"
 
 sensor_pin = 17
-RPL.pinMode(17,RPL.INPUT)
+RPL.pinMode(sensor_pin,RPL.INPUT)
 
 while True:
   PTW.state['d1'] = RPL.digitalRead(17)
-  PTW.state['d2'] = RPL.digitalRead(23)
   PTW.post()
